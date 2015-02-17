df317536099bdf6892067817e1b56ee09ea976ea
sachacohenfr <scohen@student.42.fr>
2015-02-17 21:34:42 +0000

    modif

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(function () {
 	$( "<img src='https://cdn.rawgit.com/sachacohenfr/shorlgead/master/postit.png' id='rever' />" ).insertAfter( ".mainContent" );
+	$('h1:contains("ACCUEIL")').remove();
 	$('.bonjour').replaceWith('<img src="https://cdn.rawgit.com/sachacohenfr/shorlgead/master/bonjour.png" id="BonjourImg" alt="Bonjour," />');
 	$('.menu ul.menuLevel1 a.menuItemLevel1Highlight, .menu ul.menuLevel2 a.menuItemLevel2Highlight').css('background-color', '#00A2E3');
 	$('.menu ul.menuLevel1 a.menuVisible:hover, .menu ul.menuLevel2 a.menuVisible:hover')
