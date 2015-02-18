3c1fcfcddd085e1e652b9a4c45c4aa5da3f926e9
sachacohenfr <scohen@student.42.fr>
2015-02-18 11:23:08 +0000

    push

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(function () {
-	$("meta[content='IE=EmulateIE7']").remove();
+	$("meta[content='IE=EmulateIE7']").attr('content', '');
 	$( "<img src='https://cdn.rawgit.com/sachacohenfr/shorlgead/master/postit.png' id='rever' />" ).insertAfter( ".mainContent" );
 	$('h1:contains("ACCUEIL")').css('opacity', '0');
 	$('.bonjour').replaceWith('<img src="https://cdn.rawgit.com/sachacohenfr/shorlgead/master/bonjour.png" id="BonjourImg" alt="Bonjour," />');
 	$('.menu ul.menuLevel1 a.menuItemLevel1Highlight, .menu ul.menuLevel2 a.menuItemLevel2Highlight').css('background-color', '#00A2E3');
 	//$('.menu ul.menuLevel1 a.menuVisible:hover, .menu ul.menuLevel2 a.menuVisible:hover')
 	$('.menuVisible').css('border-radius', '0px');
+	$('.mainContent').attr('bgcolor', '#FFFFFF');
 	$('.menuPosition').css('background-image', 'url(https://cdn.rawgit.com/sachacohenfr/shorlgead/master/bg-menu.png)');
 	$('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/sachacohenfr/shorlgead/master/style.css">');
 	$('head').append("<link href=//fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>");
